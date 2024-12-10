70f97e332e7e4b253cf7e57900a3511aecce6d7c
Sidra-Momin <sidraiaman7411@gmail.com>
2024-12-10 12:31:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add smooth scrolling to all internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.query
